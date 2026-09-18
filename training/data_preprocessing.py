ab904827592e9faddebdaa8b752c89f41c41d25a
Abhishek-surya <abhisheksuryavanshi220@gmail.com>
2026-09-19 01:31:07 +0530

    Add dataset validation

--- training/data_preprocessing.py
+++ training/data_preprocessing.py
@@ -12,3 +12,30 @@ print(df.head())
 
 print("\nData types:")
 print(df.dtypes)
+
+# Assign column names
+df.columns = [
+    "age",
+    "sex",
+    "cp",
+    "trestbps",
+    "chol",
+    "fbs",
+    "restecg",
+    "thalach",
+    "exang",
+    "oldpeak",
+    "slope",
+    "ca",
+    "thal",
+    "target"
+]
+
+print("\nColumn names:")
+print(df.columns)
+
+print("\nMissing values:")
+print((df == "?").sum())
+
+print("\nDuplicate rows:")
+print(df.duplicated().sum())
